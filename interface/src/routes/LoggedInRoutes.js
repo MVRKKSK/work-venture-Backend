712e5010679bcc06c389da949d2053e26fc1103d
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-09-21 16:16:23 +0530

    first commit

--- interface/src/routes/LoggedInRoutes.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux"
-import { Outlet } from "react-router-dom";
-import Login from "../components/authentication/Login";
-
-export const LoggedInRoutes = () => {
-    const { user } = useSelector((state) => ({...state }));
-    return user ? < Outlet / > : < Login / >
-}
